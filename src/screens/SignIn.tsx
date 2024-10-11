1f08dca82e880dfc79fa9c610ea6a35503907fb9
douglasbertelli <73892745+douglasbertelli@users.noreply.github.com>
2024-10-11 14:26:13 -0300

    Componente Input
    1. Construção do componente de Input.
    2. Finalização da tela de SignIn.

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,25 +1,40 @@
-import {VStack, Text, Image, Center} from "@gluestack-ui/themed";
+import { VStack, Text, Image, Center, Heading } from "@gluestack-ui/themed";
 
-import BackgroundImg from "@assets/background.png"
-import Logo from "@assets/logo.svg"
+import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export default function SignIn() {
-	return (
-		<VStack flex={1} bg={'$gray700'} alignItems="center">
-			<Image
-				w={"$full"}
-				h={624}
-				source={BackgroundImg}
-				defaultSource={BackgroundImg}
-				alt={'Pessoas treinando'}
-				position={'absolute'}
-			/>
-			<Center my={'$24'} gap={'$3'}>
-				<Logo/>
-				<Text>
-					Olá mundo...
-				</Text>
-			</Center>
-		</VStack>
-	)
+  return (
+    <VStack flex={1} bg={"$gray700"} alignItems="center">
+      <Image
+        w={"$full"}
+        h={624}
+        source={BackgroundImg}
+        defaultSource={BackgroundImg}
+        alt={"Pessoas treinando"}
+        position={"absolute"}
+      />
+
+      <VStack flex={1} padding={"$6"} pb={"$16"} w={"$full"}>
+        <Center my={"$24"} gap={"$3"}>
+          <Logo />
+          <Text color="$gray100">Treine seu corpo e sua mente.</Text>
+        </Center>
+
+        <Center gap={"$3"}>
+          <Heading color="$gray100">Acesse sua conta</Heading>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder="Senha"
+            secureTextEntry
+          />
+        </Center>
+      </VStack>
+    </VStack>
+  );
 }
